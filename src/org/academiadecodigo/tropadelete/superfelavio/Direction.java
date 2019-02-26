f8ec6a57c4a89a2723dc0e271bc4c474b92b1321
Sandra Goncalves <sandra.leo.goncalves@gmail.com>
2019-02-26 15:52:11 -0100

    update class direction

--- src/org/academiadecodigo/tropadelete/superfelavio/Direction.java
+++ src/org/academiadecodigo/tropadelete/superfelavio/Direction.java
@@ -10,10 +10,34 @@ public enum Direction {
     LEFT;
 
     public static Direction keysToDirection (KeyboardEvent key) {
+
+        switch (key.getKey()) {
+            case KeyboardEvent.KEY_SPACE:
+                return UP;
+            case KeyboardEvent.KEY_RIGHT:
+                return RIGHT;
+            case KeyboardEvent.KEY_LEFT:
+                return LEFT;
+        }
+
         return null;
+
     }
 
     public static Direction oppositeDirection (Direction currentDirection){
-        return null;
+
+        Direction opposite = null;
+
+        switch (currentDirection) {
+            case RIGHT:
+                opposite = Direction.LEFT;
+                break;
+            case LEFT:
+                opposite = Direction.RIGHT;
+                break;
+        }
+
+        return opposite;
+
     }
 }
