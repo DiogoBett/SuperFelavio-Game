aa340dc725c0eeebfd47a515018f4542aba7f851
Joao Vieira <joaosvieira97@gmail.com>
2019-02-26 14:08:19 -0100

    Keyboard Listener with key press e key release a funcionar

--- src/org/academiadecodigo/tropadelete/superfelavio/KeyboardListener.java
+++ src/org/academiadecodigo/tropadelete/superfelavio/KeyboardListener.java
@@ -1,24 +1,51 @@
 package org.academiadecodigo.tropadelete.superfelavio;
 
+import org.academiadecodigo.simplegraphics.keyboard.Keyboard;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEventType;
 import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
-import org.academiadecodigo.tropadelete.superfelavio.gameObjects.GameObject;
 import org.academiadecodigo.tropadelete.superfelavio.gameObjects.Player;
 
+
+
 public class KeyboardListener implements KeyboardHandler {
+    private static final int[] controls = {
+            KeyboardEvent.KEY_RIGHT,
+            KeyboardEvent.KEY_LEFT,
+            KeyboardEvent.KEY_SPACE
+    };
+    private Player felaveo;
 
     public KeyboardListener (Player movable){
+        felaveo = movable;
 
+        Keyboard keyboard = new Keyboard(this);
+        for (int i = 0 ; i < controls.length ; i++){
+            subscribe(controls[i],keyboard,KeyboardEventType.KEY_PRESSED);
+            subscribe(controls[i],keyboard,KeyboardEventType.KEY_RELEASED);
+
+        }
     }
+
+    private void subscribe (int key , Keyboard keyboard,KeyboardEventType typeEvent){
+        KeyboardEvent event = new KeyboardEvent();
+        event.setKey(key);
+        event.setKeyboardEventType(typeEvent);
+        keyboard.addEventListener(event);
+    }
+
+
 
 
     @Override
     public void keyPressed(KeyboardEvent keyboardEvent) {
+     felaveo.setCurrentDirection(Direction.keysToDirection(keyboardEvent));
 
     }
 
     @Override
     public void keyReleased(KeyboardEvent keyboardEvent) {
+        felaveo.setCurrentDirection(null);
 
     }
 }
